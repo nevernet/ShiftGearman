2e9bc2e3080475552e0414bf40653b066f13ef04
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2012-10-30 13:43:52 +0400

    Added some more unit tests for gearman service.

--- tests/unit/ShiftGearman/GearmanServiceTest.php
+++ tests/unit/ShiftGearman/GearmanServiceTest.php
@@ -114,5 +114,91 @@ class GearmanServiceTest extends TestCase
     }
 
 
+    /**
+     * Test that we do throw an exception when requesting worker without
+     * configuration.
+     *
+     * @test
+     * @expectedException \ShiftGearman\Exception\ConfigurationException
+     * @expectedExceptionMessage Worker 'me-not configured' is not configured
+     */
+    public function throwExceptionWhenRequestingWorkerWithoutConfig()
+    {
+        $service = new GearmanService($this->getLocator());
+        $service->setConfig(array());
+        $service->getWorker('me-not configured');
+    }
+
+    /**
+     * Test that we do throw an exception if worker requests connection that
+     * is not configured.
+     *
+     * @test
+     * @expectedException \ShiftGearman\Exception\ConfigurationException
+     * @expectedExceptionMessage Connection 'not-configured' requested by worker
+     * 'worka' is not configured
+     */
+    public function throwExceptionWhenWorkerHasNonexistentConnectionConfigured()
+    {
+        $config = array(
+            'connections' => array(),
+            'workers' => array(
+                'worka' => array(
+                    'connection' => 'not-configured',
+                    'jobs' => array()
+                )
+            )
+        );
+
+
+        $service = new GearmanService($this->getLocator());
+        $service->setConfig($config);
+        $service->getWorker('worka');
+    }
+
+
+    /**
+     * Test that we are able to get configured worker by name.
+     * @test
+     */
+    public function canGetWorker()
+    {
+        //prepare config
+        $config = array(
+            'connections' => array(
+                'default' => array(
+                    'timeout' => null,
+                    'servers' => array(
+                        array('host' => '127.0.0.1', 'port' => 4730)
+                    )
+                )
+            ),
+            'workers' => array(
+                'worka' => array(
+                    'connection' => 'default',
+                    'jobs' => array()
+                )
+            )
+        );
+
+        //mock worker
+        $worker = Mockery::mock('ShiftGearman\Worker\Worker');
+        $worker->shouldReceive('setConfig')
+            ->with($config['workers']['worka']);
+        $worker->shouldReceive('setConnectionConfig')
+            ->with($config['connections']['default']);
+
+        //mock locator
+        $locator = Mockery::mock('Zend\Di\Locator');
+        $locator->shouldReceive('newInstance')
+            ->with('ShiftGearman\Worker\Worker')
+            ->andReturn($worker);
+
+        $service = new GearmanService($locator);
+        $service->setConfig($config);
+        $this->assertEquals($worker, $service->getWorker('worka'));
+    }
+
+
 
 }//class ends here
