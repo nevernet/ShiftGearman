603757c792fbefdc95c0989e0e35cadb79dc88ee
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2012-11-06 14:29:15 +0400

    Removing test gearman controller.

--- config/routes.php
+++ config/routes.php
@@ -1,18 +1,3 @@
 <?php
 return array(
-
-/*
- * Gearman testing route
- */
-'gearman' => array(
-    'type'    => 'Zend\Mvc\Router\Http\Literal',
-    'options' => array(
-        'route'    => '/gearman/',
-        'defaults' => array(
-            'controller' => 'shiftgearman-controller-gearman',
-            'action'     => 'index',
-        ),
-    ),
-),
-
 );
